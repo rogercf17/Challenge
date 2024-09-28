a8a7f8436396a49fb00be9bd5eebdce8f75d77a8
rogercf17 <rogercf16@gmail.com>
2024-09-27 21:38:38 -0300

    Implementação de rotas finalizada

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,35 +1,34 @@
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
-    return(
-        <>
-            <header className="
-                w-screen h-24
-                flex justify-around items-center
-                border-b border-solid	 border-slate-700	
-                "
-            >
-                <h1>Formula E</h1>
-                <nav>
-                    <ul className="flex gap-5">
-                        <li>
-                            <NavLink to='/pilotosequipes'>Pilotos e Equipes</NavLink>
-                        </li>
-                        <li>
-                            <NanLink to='/estatisticas'>Estatísticas</NanLink>
-                        </li>
-                        <li>
-                            <NanLink to='/informacoes'>Informações</NanLink>
-                        </li>
-                        <li>
-                            <NanLink to='/interativos'>Interativos</NanLink>
-                        </li>
-                    </ul>
-                </nav>
-                <div>
-                    <button className="bg-slate-800 h-11 w-20 rounded-md">Login</button>
-                </div>
-            </header>
-        </>
-    )
+    return (
+        <header className="
+            w-screen h-24
+            flex justify-around items-center
+            border-b border-solid border-slate-700
+        ">  
+            <NavLink to='/' id="logo">
+                <h1>Portal E</h1>
+            </NavLink>
+            <nav>
+                <ul className="flex gap-5">
+                    <li>
+                        <NavLink to='/pilotosequipes'>Pilotos e Equipes</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/estatisticas'>Estatísticas</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/informacoes'>Informações</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/interativos'>Interativos</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div>
+                <button className="bg-slate-800 h-11 w-20 rounded-md">Login</button>
+            </div>
+        </header>
+    );
 }
--- src/pages/Informacoes.jsx
+++ src/pages/Informacoes.jsx
@@ -1,7 +1,68 @@
 export default function Informacoes() {
-    return(
-        <>
-            <h1>Informações</h1>
-        </>
-    )
+  return (
+    <section
+      className="
+        w-10/12
+        grid grid-rows-3
+        justify-center items-center	mx-auto
+    "
+    >
+      <div
+        className="
+        bg-cyan-800 
+        mt-2.5 p-3
+        rounded-2xl
+      "
+      >
+        <h1 className="font-bold antialiased text-4xl mb-1.5">História</h1>
+        <p className="text-base text-justify leading-6">
+          O campeonato de Fórmula E (Fórmula E) foi concebido em 2011 como uma
+          alternativa sustentável e ambientalmente amigável às corridas
+          tradicionais de rodas abertas. A ideia era criar uma plataforma para
+          veículos elétricos competirem em uma série de corridas de alto nível,
+          promovendo a inovação e o desenvolvimento na mobilidade eletrônica.
+          Após vários anos de planejamento e desenvolvimento, a primeira corrida
+          de Fórmula E ocorreu em 13 de setembro de 2014, em Pequim, China, com
+          o apoio da Fédération Internationale de lucitaAutomobile (FIA).
+        </p>
+        <h2 className="font-bold text-xl my-1">
+          Primeira Temporada e Crescimento
+        </h2>
+        <p className="text-base text-justify leading-6">
+          A temporada inaugural contou com 20 pilotos de 10 equipes, com Audi
+          Sport ABT Schaeffler e Andretti Autosport sendo duas das equipes de
+          destaque. O piloto brasileiro Lucas di Grassi venceu a primeira
+          corrida da Fórmula E, e o campeonato foi ganho por Nelson Piquet Jr.
+          Desde então, a série cresceu, com cada temporada introduzindo novas
+          equipes, pilotos e tecnologias.
+        </p>
+        <h2 className="font-bold text-xl my-1">Principais Marcos</h2>
+        <ul className="list-disc px-5">
+            <li>
+                2015: A Fórmula E se expande para 11 cidades, com a adição de
+                Miami e Long Beach.
+            </li>
+            <li>
+                2016: O campeonato apresenta o seu primeiro evento de duas cabeças
+                em Londres.
+            </li>
+            <li>
+                2017: A Fórmula E celebra a sua 50a corrida eletrónica e introduz
+                o sistema “FanBoost”, permitindo aos fãs votar nos seus pilotos
+                favoritos.
+            </li>
+            <li>
+                2018: A série atinge um marco de 100 corridas eletrónicas e
+                introduz o sistema “Attack Mode”, permitindo que os condutores
+                aumentem temporariamente a sua potência.
+            </li>
+            <li>
+                2020: A Fórmula E se torna o Campeonato Mundial de Fórmula E da
+                FIA, com a introdução de um novo sistema de pontos e um calendário
+                revisado.
+            </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
