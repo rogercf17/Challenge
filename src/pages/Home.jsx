0df8e99e01fa762e67e238b15a4ca49596078857
rogercf17 <rogercf16@gmail.com>
2024-10-07 14:11:54 -0300

    BugFix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,90 +1,55 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
     return(
-        // <div className="flex flex-col items-center justify-center w-10/12 p-10 my-2 gap-10 mx-auto">
-        //     <div className="
-        //         w-full h-32 
-        //         p-5
-        //         shadow-2xl 
-        //         cursor-pointer
-        //         bg-slate-800
-        //         rounded-3xl
-        //         transition-all
-        //         hover:text-right
-        //         hover:scale-105
-        //     ">
-        //         <h2 className="text-3xl font-bold mb-1">Pilotos e Equipes</h2>
-        //         <p className="text-base">Conheça todos os pilotos e equipes</p>
-        //     </div>
-        //     <div className="
-        //         w-full h-32 
-        //         p-5
-        //         shadow-2xl 
-        //         cursor-pointer
-        //         bg-slate-800
-        //         rounded-3xl
-        //         hover:text-right
-        //         hover:scale-105
-        //     ">
-        //         <h2 className="text-3xl font-bold mb-1">Estatísticas</h2>
-        //         <p className="text-base">Veja a classificação da atual temporada</p>
-        //     </div>
-        //     <div className="
-        //         w-full h-32 
-        //         p-5
-        //         shadow-2xl 
-        //         cursor-pointer
-        //         bg-slate-800
-        //         rounded-3xl
-        //         hover:text-right
-        //         hover:scale-105
-        //     ">
-        //         <h2 className="text-3xl font-bold mb-1">Informações</h2>
-        //         <p className="text-base">Conheça mais sobre a Formula E</p>
-        //     </div>
-        // </div>
         <div className="flex flex-col items-center justify-center w-10/12 p-5 my-2 gap-5 mx-auto">
-            <div className="
-                w-full h-32 
-                p-5
-                shadow-2xl 
-                cursor-pointer
-                bg-slate-800
-                rounded-3xl
-                transition-all
-                hover:text-right
-                hover:scale-105
-            ">
-                <h2 className="text-2xl md:text-3xl font-bold mb-1">Pilotos e Equipes</h2>
-                <p className="text-sm md:text-base">Conheça todos os pilotos e equipes</p>
-            </div>
-            <div className="
-                w-full h-32 
-                p-5
-                shadow-2xl 
-                cursor-pointer
-                bg-slate-800
-                rounded-3xl
-                transition-all
-                hover:text-right
-                hover:scale-105
-            ">
-                <h2 className="text-2xl md:text-3xl font-bold mb-1">Estatísticas</h2>
-                <p className="text-sm md:text-base">Veja a classificação da atual temporada</p>
-            </div>
-            <div className="
-                w-full h-32 
-                p-5
-                shadow-2xl 
-                cursor-pointer
-                bg-slate-800
-                rounded-3xl
-                transition-all
-                hover:text-right
-                hover:scale-105
-            ">
-                <h2 className="text-2xl md:text-3xl font-bold mb-1">Informações</h2>
-                <p className="text-sm md:text-base">Conheça mais sobre a Formula E</p>
-            </div>
+            <Link to="/pilotosequipes" className="w-full">
+                <div className="
+                    w-full h-32 
+                    p-5
+                    shadow-2xl 
+                    cursor-pointer
+                    bg-slate-800
+                    rounded-3xl
+                    transition-all
+                    hover:scale-105
+                ">
+                    <h2 className="text-2xl md:text-3xl font-bold mb-1">Pilotos e Equipes</h2>
+                    <p className="text-sm md:text-base">Conheça todos os pilotos e equipes</p>
+                </div>
+            </Link>
+
+            <Link to="/estatisticas" className="w-full">
+                <div className="
+                    w-full h-32 
+                    p-5
+                    shadow-2xl 
+                    cursor-pointer
+                    bg-slate-800
+                    rounded-3xl
+                    transition-all
+                    hover:scale-105
+                ">
+                    <h2 className="text-2xl md:text-3xl font-bold mb-1">Estatísticas</h2>
+                    <p className="text-sm md:text-base">Veja a classificação da atual temporada</p>
+                </div>
+            </Link>
+           
+            <Link to="/informacoes" className="w-full">
+                <div className="
+                    w-full h-32 
+                    p-5
+                    shadow-2xl 
+                    cursor-pointer
+                    bg-slate-800
+                    rounded-3xl
+                    transition-all
+                    hover:scale-105
+                ">
+                    <h2 className="text-2xl md:text-3xl font-bold mb-1">Informações</h2>
+                    <p className="text-sm md:text-base">Conheça mais sobre a Formula E</p>
+                </div>
+            </Link>
         </div>
     )
 }
